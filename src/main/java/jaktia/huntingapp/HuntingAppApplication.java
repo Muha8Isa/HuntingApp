0e850502f007879553ed708bb024aca46816fe88
Mohammad Alissa <mohammad.alissa95@hotmail.com>
2023-04-27 18:24:04 +0200

    Initial commit

---
+++ src/main/java/jaktia/huntingapp/HuntingAppApplication.java
@@ -0,0 +1,13 @@
+package jaktia.huntingapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HuntingAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HuntingAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/jaktia/huntingapp/HuntingAppApplicationTests.java
@@ -0,0 +1,13 @@
+package jaktia.huntingapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HuntingAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
